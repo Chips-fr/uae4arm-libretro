35ebcd2aee2a3dd604d98bb12e9057a1d6db65d0
r-type <rtype@email.com>
2020-05-18 16:41:45 +0200

    fix

--- libretro/osdep/thread.h
+++ libretro/osdep/thread.h
@@ -1,56 +1,106 @@
  /*
   * UAE - The Un*x Amiga Emulator
-  * 
-  * Threading support, using SDL
-  * 
-  * Copyright 1997, 2001 Bernd Schmidt
+  *
+  * Threading support, using pthreads
+  *
+  * Copyright 1997 Bernd Schmidt
+  * Copyright 2004 Richard Drummond
   */
 
-#include "SDL.h"
-#include "SDL_thread.h"
+#include <pthread.h>
+#include <semaphore.h>
 
-/* Sempahores. We use POSIX semaphores; if you are porting this to a machine
- * with different ones, make them look like POSIX semaphores. */
-typedef SDL_sem *uae_sem_t;
+//#define USE_NAMED_SEMAPHORES
+#define 	SEM_FAILED   ((sem_t *) 0)
 
-#define uae_sem_init(PSEM, DUMMY, INIT) do { \
-    *PSEM = SDL_CreateSemaphore (INIT); \
-} while (0)
-#define uae_sem_destroy(PSEM) SDL_DestroySemaphore (*PSEM)
-#define uae_sem_post(PSEM) SDL_SemPost (*PSEM)
-#define uae_sem_wait(PSEM) SDL_SemWait (*PSEM)
-#define uae_sem_trywait(PSEM) SDL_SemTryWait (*PSEM)
-#define uae_sem_getvalue(PSEM) SDL_SemValue (*PSEM)
+typedef struct {
+    sem_t *sem;
+} uae_sem_t;
+
+#ifndef USE_NAMED_SEMAPHORES
+
+int uae_sem_init (uae_sem_t *sem, int pshared, unsigned int value);
+
+STATIC_INLINE int uae_sem_destroy (uae_sem_t *sem)
+{
+    return sem_destroy (sem->sem);
+}
+
+STATIC_INLINE int uae_sem_post (uae_sem_t *sem)
+{
+    return sem_post (sem->sem);
+}
+
+STATIC_INLINE int uae_sem_wait (uae_sem_t *sem)
+{
+    return sem_wait (sem->sem);
+}
+
+STATIC_INLINE int uae_sem_trywait (uae_sem_t *sem)
+{
+    return sem_trywait (sem->sem);
+}
+
+STATIC_INLINE int uae_sem_getvalue (uae_sem_t *sem, int *sval)
+{
+    return sem_getvalue (sem->sem, sval);
+}
+
+#else
+
+int uae_sem_init (uae_sem_t *sem, int pshared, unsigned int value);
+
+STATIC_INLINE int uae_sem_destroy (uae_sem_t *sem)
+{
+    return sem->sem == 0 ? -1 : sem_close (sem->sem);
+}
+
+STATIC_INLINE int uae_sem_post (uae_sem_t *sem)
+{
+    return sem->sem == 0 ? -1 : sem_post (sem->sem);
+}
+
+STATIC_INLINE int uae_sem_wait (uae_sem_t *sem)
+{
+    return sem->sem == 0 ? -1 : sem_wait (sem->sem);
+}
+
+STATIC_INLINE int uae_sem_trywait (uae_sem_t *sem) {
+    return sem->sem == 0 ? -1 : sem_trywait (sem->sem);
+}
+
+STATIC_INLINE int uae_sem_getvalue (uae_sem_t *sem, int *sval)
+{
+    return sem->sem == 0 ? -1 : sem_getvalue (sem->sem, sval);
+}
+
+#endif
 
 #include "commpipe.h"
-//extern void uae_set_thread_priority (int);
+
+typedef pthread_t uae_thread_id;
+#define BAD_THREAD 0
+
+STATIC_INLINE int uae_start_thread (char *name, void *(*f) (void *), void *arg, uae_thread_id *foo)
+{
+	int result;
+	result = pthread_create (foo, 0, f, arg);
+
+	return 0 == result;
+}
+
+STATIC_INLINE int uae_wait_thread (uae_thread_id thread)
+{
+    return pthread_join (thread, (void**)0);
+}
+
+STATIC_INLINE void uae_kill_thread (uae_thread_id* thread)
+{
+	pthread_detach(*thread);
+}
+
+
+#define UAE_THREAD_EXIT pthread_exit(0)
+
 #define uae_set_thread_priority(pri)
 
-typedef SDL_Thread *uae_thread_id;
-#define BAD_THREAD NULL
-
-STATIC_INLINE int uae_start_thread (const char *name, void *(*f) (void *), void *arg, uae_thread_id *foo)
-{
-  uae_thread_id id = SDL_CreateThread ((int (*)(void *))f, arg);
-  if(foo != NULL)
-    *foo = id;
-  write_log("uae_start_thread: %s, id=%d\n", name, id);
-  return id == 0;
-}
-
-STATIC_INLINE int uae_start_thread_fast (void *(*f) (void *), void *arg, uae_thread_id *foo)
-{
-  uae_thread_id id = SDL_CreateThread ((int (*)(void *))f, arg);
-  if(foo != NULL)
-    *foo = id;
-  write_log("uae_start_thread_fast: arg=0x%08X, id=%d\n", arg, id);
-  return id == 0;
-}
-
-STATIC_INLINE void uae_wait_thread (uae_thread_id thread)
-{
-    SDL_WaitThread (thread, (int*)0);
-}
-
-/* Do nothing; thread exits if thread function returns.  */
-#define UAE_THREAD_EXIT do {} while (0)
